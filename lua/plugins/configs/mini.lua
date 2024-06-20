383b2c1dda403056d72753516b629135099b9fff
Q Xu <xuq3196@outlook.com>
2024-06-20 10:29:11 +0800

    enable mini session manager

--- lua/plugins/configs/mini.lua
+++ lua/plugins/configs/mini.lua
@@ -37,22 +37,22 @@ return { -- Collection of various small independent plugins/modules
     -- minimap key mappings
 
     local map = vim.keymap.set
-    map('n', '<Leader>mo', function()
+    map('n', '<Leader>mmo', function()
       MiniMap.open()
     end, { desc = 'MiniMap Open Map' })
-    map('n', '<Leader>mc', function()
+    map('n', '<Leader>mmc', function()
       MiniMap.close()
     end, { desc = 'MiniMap Close Map' })
-    map('n', '<Leader>mt', function()
+    map('n', '<Leader>mmt', function()
       MiniMap.toggle()
     end, { desc = 'MiniMap Toggle map' })
-    map('n', '<Leader>mf', function()
+    map('n', '<Leader>mmf', function()
       MiniMap.toggle_focus()
     end, { desc = 'MiniMap Toggle focus' })
-    map('n', '<Leader>mr', function()
+    map('n', '<Leader>mmr', function()
       MiniMap.refresh()
     end, { desc = 'MiniMap Refresh Map' })
-    map('n', '<Leader>ms', function()
+    map('n', '<Leader>mms', function()
       MiniMap.toggle_side()
     end, { desc = 'MiniMap Toggle side' })
 
@@ -71,6 +71,44 @@ return { -- Collection of various small independent plugins/modules
       return '%2l:%-2v'
     end
 
+    require('mini.sessions').setup {
+      autowrite = false,
+    }
+    -- mappings for mini sessions
+    local function get_session_name()
+      local md5 = require 'libs.md5'
+      local cwd = vim.fn.getcwd()
+      local name = md5.sumhexa(cwd)
+      name = 'session_' .. name
+      return name
+    end
+    local function session_exist(session_name)
+      local session_dir = MiniSessions.config.directory
+      local session_file = session_dir .. '/' .. session_name
+      return vim.fn.filereadable(session_file) == 1
+    end
+    map('n', '<Leader>msw', function()
+      local session_name = get_session_name()
+      MiniSessions.write(session_name)
+    end, { desc = 'MiniSessions Save Session' })
+    map('n', '<Leader>msr', function()
+      local session_name = get_session_name()
+      if session_exist(session_name) then
+        MiniSessions.read(session_name)
+      else
+        print 'Session does not exist'
+        return
+      end
+    end, { desc = 'MiniSessions read Session' })
+    map('n', '<Leader>msd', function()
+      local session_name = get_session_name()
+      if session_exist(session_name) then
+        MiniSessions.delete(session_name, { force = true })
+      else
+        print 'Session does not exist'
+        return
+      end
+    end, { desc = 'MiniSessions delete Session' })
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
   end,
