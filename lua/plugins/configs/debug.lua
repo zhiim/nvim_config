ebabef49f6ddadb64076f5a1fa205ad10c44043c
Q Xu <xuq3196@outlook.com>
2024-08-26 14:20:48 +0800

    add key mapping to toggle debug ui

--- lua/plugins/configs/debug.lua
+++ lua/plugins/configs/debug.lua
@@ -71,9 +71,7 @@ return {
         },
       },
     }
-
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+    vim.keymap.set('n', '<F6>', dapui.toggle(), { desc = 'Toggle debug UI.' })
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
