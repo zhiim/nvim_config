a494b25aebd58117121db0fe54e3e423be4a9c06
Q Xu <xuq3196@outlook.com>
2024-11-03 11:18:13 +0800

    add usefule copilot chat settings

--- lua/plugins/configs/copilot.lua
+++ lua/plugins/configs/copilot.lua
@@ -5,19 +5,47 @@ return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
     branch = 'canary',
+    cmd = 'CopilotChatModels',
     keys = {
       {
         '<leader>ct',
         function()
+          require('CopilotChat').toggle()
+        end,
+        mode = 'n',
+        desc = 'CopilotChat Toggle',
+      },
+      {
+        '<leader>cs',
+        '<cmd>CopilotChatModels<cr>',
+        mode = 'n',
+        desc = 'CopilotChat Select Models',
+      },
+      {
+        '<leader>cq',
+        function()
+          local input = vim.fn.input 'Quick Chat: '
+          if input ~= '' then
+            require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
+          end
+        end,
+        desc = 'CopilotChat Quick Chat',
+      },
+      {
+        '<leader>ci',
+        function()
           require('CopilotChat').toggle {
             window = {
               layout = 'float',
-              title = 'Copilot Chat',
+              relative = 'cursor',
+              width = 1,
+              height = 0.4,
+              row = 1,
             },
           }
         end,
         mode = 'n',
-        desc = 'Copilot Chat Toggle ',
+        desc = 'CopilotChat Inline Chat',
       },
     },
     dependencies = {
@@ -34,29 +62,23 @@ return {
               },
             },
           }
-          vim.keymap.set('n', '<leader>cp', function()
+          vim.keymap.set('n', '<leader>co', function()
             require('copilot.panel').open()
-          end, { desc = 'Open Copilot Panel' })
+          end, { desc = 'Open Copilot Open Panel' })
         end,
       },
       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
     },
-    opts = {
-      -- See Configuration section for rest
-      window = {
-        layout = 'float', -- 'vertical', 'horizontal', 'float', 'replace'
-        width = 0.8, -- fractional width of parent, or absolute width in columns when > 1
-        height = 0.8, -- fractional height of parent, or absolute height in rows when > 1
-        -- Options below only apply to floating windows
-        relative = 'editor', -- 'editor', 'win', 'cursor', 'mouse'
-        border = 'single', -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
-        row = nil, -- row position of the window, default is centered
-        col = nil, -- column position of the window, default is centered
-        title = 'Copilot Chat', -- title of chat window
-        footer = nil, -- footer of chat window
-        zindex = 1, -- determines if window is on top or below other floating windows
-      },
-    },
-    -- See Commands section for default commands if you want to lazy load on them
+    config = function()
+      require('CopilotChat').setup {
+        debug = true,
+        mappings = {
+          complete = {
+            insert = '',
+          },
+        },
+      }
+      require('CopilotChat.integrations.cmp').setup()
+    end,
   },
 }
