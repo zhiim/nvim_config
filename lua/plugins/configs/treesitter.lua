34bf0e41742016d08c161c152cfd4a693ce59d9d
Q Xu <xuq3196@outlook.com>
2024-11-12 17:14:07 +0800

    update treesitter

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -10,16 +10,14 @@ else
         -- NOTE: language parsers need to be installed
         --
         ensure_installed = { 'bash', 'c', 'cpp', 'python', 'diff', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc' },
-        ignore_install = { 'latex' },
         -- Autoinstall languages that are not installed
         auto_install = false,
         highlight = {
           enable = true,
-          disable = { 'latex' },
           -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
           --  If you are experiencing weird indenting issues, add the language to
           --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-          additional_vim_regex_highlighting = { 'ruby' },
+          additional_vim_regex_highlighting = { 'ruby', 'latex' },
         },
         indent = { enable = true, disable = { 'ruby' } },
       },
