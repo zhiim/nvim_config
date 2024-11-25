7c22db6c0c588e83fe8ddcf30c40494c9a6ab4ff
Q Xu <xuq3196@outlook.com>
2024-11-25 19:10:38 +0800

    set mini.icons as core plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -262,6 +262,17 @@ require('lazy').setup({
     },
   },
 
+  {
+    'echasnovski/mini.icons',
+    lazy = true,
+    init = function()
+      package.preload['nvim-web-devicons'] = function()
+        require('mini.icons').mock_nvim_web_devicons()
+        return package.loaded['nvim-web-devicons']
+      end
+    end,
+  },
+
   require 'plugins.configs.ui',
   require 'plugins.configs.language',
   require 'plugins.configs.git',
