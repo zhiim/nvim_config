e36b44d3b9cbdbd264fb0d82e9016223893a5416
Q Xu <xuq3196@outlook.com>
2024-12-03 10:53:06 +0800

    update mini.animate using lazyvim's config

--- lua/plugins/configs/ui/enhance.lua
+++ lua/plugins/configs/ui/enhance.lua
@@ -5,12 +5,43 @@ if vim.g.options.enhance then
       'echasnovski/mini.animate',
       event = 'VeryLazy',
       config = function()
+        -- don't use animate when scrolling with the mouse
+        local mouse_scrolled = false
+        for _, scroll in ipairs { 'Up', 'Down' } do
+          local key = '<ScrollWheel' .. scroll .. '>'
+          vim.keymap.set({ '', 'i' }, key, function()
+            mouse_scrolled = true
+            return key
+          end, { expr = true })
+        end
+
+        vim.api.nvim_create_autocmd('FileType', {
+          pattern = 'grug-far',
+          callback = function()
+            vim.b.minianimate_disable = true
+          end,
+        })
+
+        local animate = require 'mini.animate'
         require('mini.animate').setup {
+          resize = {
+            timing = animate.gen_timing.linear { duration = 50, unit = 'total' },
+          },
           scroll = {
-            enable = false,
+            timing = animate.gen_timing.linear { duration = 150, unit = 'total' },
+            subscroll = animate.gen_subscroll.equal {
+              predicate = function(total_scroll)
+                if mouse_scrolled then
+                  mouse_scrolled = false
+                  return false
+                end
+                return total_scroll > 1
+              end,
+            },
           },
           cursor = {
-            enable = false,
+            -- enable = false,
+            timing = animate.gen_timing.exponential { duration = 50, unit = 'total' },
           },
         }
       end,
