92bc9ff05b8997f71190e848cbc7257661ca3851
Q Xu <xuq3196@outlook.com>
2025-02-14 18:15:11 +0800

    update tiny-diagnotics

--- lua/plugins/configs/ui/enhance.lua
+++ lua/plugins/configs/ui/enhance.lua
@@ -58,16 +58,9 @@ if vim.g.options.enhance then
 
     {
       'rachartier/tiny-inline-diagnostic.nvim',
-      event = function()
-        if vim.fn.has 'nvim-0.10' then
-          return 'LspAttach'
-        else
-          return 'VeryLazy'
-        end
-      end,
+      event = 'VeryLazy',
       priority = 800, -- needs to be loaded in first
       config = function()
-        vim.diagnostic.config { virtual_text = false }
         -- Default configuration
         require('tiny-inline-diagnostic').setup {
           signs = {
@@ -89,21 +82,24 @@ if vim.g.options.enhance then
             mixing_color = require('utils.util').get_hl('Normal').bg,
           },
           blend = {
-            factor = 0.1,
+            factor = 0.08,
           },
           options = {
             -- Show the source of the diagnostic.
             show_source = true,
-            -- If multiple diagnostics are under the cursor, display all of them.
-            multiple_diag_under_cursor = true,
-            -- Enable diagnostic message on all lines.
-            multilines = true,
+            use_icons_from_diagnostic = true,
+            multilines = {
+              enabled = true,
+            },
             -- Show all diagnostics on the cursor line.
             show_all_diags_on_cursorline = true,
-            -- Enable diagnostics on Insert mode. You should also se the `throttle` option to 0, as some artefacts may appear.
-            enable_on_insert = false,
+            virt_texts = {
+              -- Priority for virtual text display
+              priority = 10000,
+            },
           },
         }
+        vim.diagnostic.config { virtual_text = false }
       end,
     },
   }
